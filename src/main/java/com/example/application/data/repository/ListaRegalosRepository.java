fc2a3cdafd7b2fc3531db3303863a241e2e5bb5f
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2024-06-07 21:29:25 +0200

    Borrando todo el contenido del repositorio

--- src/main/java/com/example/application/data/repository/ListaRegalosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.application.data.repository;
-
-import com.example.application.data.entity.ListaRegalos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-public interface ListaRegalosRepository extends JpaRepository<ListaRegalos, Long>, JpaSpecificationExecutor<ListaRegalos> {
-
-}
--- src/main/java/com/example/application/data/repository/RegaloRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.application.data.repository;
-
-import com.example.application.data.entity.Regalo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-public interface RegaloRepository extends JpaRepository<Regalo, Long>, JpaSpecificationExecutor<Regalo> {
-
-}
